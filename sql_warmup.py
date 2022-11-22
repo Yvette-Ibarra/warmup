ef763e869cbaf87c465dbf1a96f9bd93a0e7dc41
Yvette Ibarra <yvette.ibarra01@gmail.com>
2022-11-22 09:49:14 -0600

    warm-up 11-22-22

--- sql_warmup.py
+++ sql_warmup.py
@@ -1,4 +1,4 @@
-Warm-up 11-9-2022
+# Warm-up 11-9-2022
 #Find the average salary of employees and the standard deviation of employees salary
 
 USE employees;
@@ -18,3 +18,26 @@ USE employees;
 SELECT avg(salary), STD(salary)
 FROM salaries
 WHERE to_date > curdate();
+
+# Warm-up 11-10-2022
+# Find the average salary per departments of current employees. (Use employees data)
+
+USE employees;
+SELECT departments.dept_name, avg(salaries.salary)
+FROM salaries
+JOIN dept_emp USING(emp_no)
+JOIN departments USING (dept_no)
+WHERE dept_emp.to_date<curdate()
+AND salaries.to_date < curdate()
+GROUP BY departments.dept_name;
+
+
+# Warm-up 11-22-2022
+## Retrieve the salary of the second highest paid current employee
+
+USE employees;
+
+SELECT salary
+FROM salaries
+WHERE to_date > curdate()
+ORDER BY salary DESC limit 1,1;
